c08c92007a97a86e0d20b0ff8822f504c6f420fe
Shubham Kumar Singh <106288561+Shubhuubxr@users.noreply.github.com>
2022-12-10 17:46:10 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,27 @@
+function home(){
+              document.getElementById("button").style.background="url(img.jpg)";
 
+              setBackground();
+}
+function work(){
+              document.getElementById("button").style.background="url(img1.jpg)";
+              setBackground();
+
+}
+function blog(){
+              document.getElementById("button").style.background="url(img2.jpg)";
+              setBackground();
+             
+}
+function about(){
+              document.getElementById("button").style.background="url(img3.jpg)";
+              setBackground();
+
+}
+
+function setBackground() {
+              document.getElementById("button").style.backgroundSize="cover";
+              document.getElementById("button").style.backgroundPosition="center"; 
+ 
+              
+}
