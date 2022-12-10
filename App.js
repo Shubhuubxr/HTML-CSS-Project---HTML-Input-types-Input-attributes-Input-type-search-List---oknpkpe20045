5b24ee5d39dd988d40f6aac316c2bf1c07575b1b
Shubham Kumar Singh <106288561+Shubhuubxr@users.noreply.github.com>
2022-12-10 18:51:08 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
 function home(){
-              document.getElementById("button").style.background="url(img.jpg)";
+    document.getElementById("button").style.background="url(https://picsum.photos/seed/picsum/200/300)";
 
-              setBackground();
+    setBackground();
 }
 function work(){
-              document.getElementById("button").style.background="url(img1.jpg)";
-              setBackground();
+    document.getElementById("button").style.background="url(https://picsum.photos/200/300.jpg)";
+    setBackground();
 
 }
 function blog(){
-              document.getElementById("button").style.background="url(img2.jpg)";
-              setBackground();
-             
+    document.getElementById("button").style.background="url(https://picsum.photos/200/300/?blur)";
+    setBackground();
+   
 }
 function about(){
-              document.getElementById("button").style.background="url(img3.jpg)";
-              setBackground();
+    document.getElementById("button").style.background="url(https://picsum.photos/id/237/200/300)";
+    setBackground();
 
 }
 
 function setBackground() {
-              document.getElementById("button").style.backgroundSize="cover";
-              document.getElementById("button").style.backgroundPosition="center"; 
- 
-              
+    document.getElementById("button").style.backgroundSize="url(https://picsum.photos/200/300)";
+    document.getElementById("button").style.backgroundPosition="center"; 
+
+    
 }
